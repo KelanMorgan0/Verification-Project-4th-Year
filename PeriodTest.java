b4cca4d07040bd717a3e86d0f14a30b33d16bb89
KelanMorgan0 <113534070+KelanMorgan0@users.noreply.github.com>
2024-10-25 15:20:10 +0100

    initial commit

---
+++ PeriodTest.java
@@ -0,0 +1,222 @@
+package cm;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeriodTest {
+
+    // Constructor tests
+    // Test 1 Valid
+    @Test
+    public void startGreaterThanZeroTest() {
+        Period p = new Period(2, 6);
+        int expected = 2;
+        int actual = p.startHour;
+
+        assertEquals(expected, actual);
+    }
+
+    // Test 2 Valid
+    @Test
+    public void startLessThanTwentyFourTest() {
+        Period p = new Period(23, 24);
+        int expected = 23;
+        int actual = p.startHour;
+
+        assertEquals(expected, actual);
+    }
+
+    // Test 3 Valid
+    @Test
+    public void startEqualToZeroTest() {
+        Period p = new Period(0, 10);
+        int expected = 0;
+        int actual = p.startHour;
+
+        assertEquals(expected, actual);
+    }
+
+    // Test 4 Invalid
+    @Test
+    public void startEqualToTwentyFourTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(24, 24);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 5 Invalid
+    @Test
+    public void startGreaterThanTwentyThreeTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(30, 31);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 6 Invalid
+    public void startLessThanZeroTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(-3, 5);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 7 Valid
+    @Test
+    public void endGreaterThanZeroTest() {
+        Period p = new Period(3, 7);
+        int expected = 7;
+        int actual = p.endHour;
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 8 Valid
+    @Test
+    public void endEqualToTwentyFourTest() {
+        Period p = new Period(3, 24);
+        int expected = 24;
+        int actual = p.endHour;
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 9 Valid
+    @Test
+    public void endLessThanTwentyFourTest() {
+        Period p = new Period(3, 14);
+        int expected = 14;
+        int actual = p.endHour;
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 10 Invalid
+    @Test
+    public void endEqualToZeroTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(0, 0);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 11 Invalid
+    @Test
+    public void endGreaterThanTwentyFiveTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(15, 26);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 12 Invalid
+    @Test
+    public void endLessThanOneTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(-2, -1);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 13 Invalid
+    @Test
+    public void endLessThanStartTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(10, 5);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 14 Invalid
+    @Test
+    public void endEqualToStartTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new Period(10, 10);
+        });
+
+        assertNull(thrown.getMessage());
+    }
+
+    //Test 15 Valid
+    @Test
+    public void endGreaterThanStartTest() {
+        Period p = new Period(10, 15);
+        boolean expected = true;
+        boolean actual = p.endHour > p.startHour;
+
+        assertEquals(expected, actual);
+    }
+
+    //Duration Tests
+    //Test 1 Valid
+    @Test
+    public void anyRangeDurationTest() {
+        Period p = new Period(10, 15);
+        int expected = 5;
+        int actual = p.duration();
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 2 Valid
+    @Test
+    public void maxRangeDurationTest() {
+        Period p = new Period(0, 24);
+        int expected = 24;
+        int actual = p.duration();
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 3 Valid
+    @Test
+    public void minRangeDurationTest() {
+        Period p = new Period(10, 11);
+        int expected = 1;
+        int actual = p.duration();
+
+        assertEquals(expected, actual);
+    }
+
+    //Overlaps Tests
+    //Test 1 Valid
+    @Test
+    public void noOverlapTest() {
+        Period p = new Period(10, 12);
+        Period p2 = new Period(15, 17);
+        boolean expected = false;
+        boolean actual = p.overlaps(p2);
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 2 Valid
+    @Test
+    public void overlapTest() {
+        Period p = new Period(10, 12);
+        Period p2 = new Period(11, 16);
+        boolean expected = true;
+        boolean actual = p.overlaps(p2);
+
+        assertEquals(expected, actual);
+    }
+
+    //Test 3 Valid
+    @Test
+    public void closeOverlapTest() {
+        Period p = new Period(10, 12);
+        Period p2 = new Period(12, 15);
+        boolean expected = false;
+        boolean actual = p.overlaps(p2);
+
+        assertEquals(expected, actual);
+    }
+}
